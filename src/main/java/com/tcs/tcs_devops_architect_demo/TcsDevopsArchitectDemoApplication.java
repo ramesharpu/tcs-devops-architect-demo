72220e5ba23839893fefa70efd3d0efc561d16f1
Ramesh A <ramesharpu@gmail.com>
2025-11-21 08:29:40 +0530

    initial commit

---
+++ src/main/java/com/tcs/tcs_devops_architect_demo/TcsDevopsArchitectDemoApplication.java
@@ -0,0 +1,21 @@
+package com.tcs.tcs_devops_architect_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class TcsDevopsArchitectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcsDevopsArchitectDemoApplication.class, args);
+	}
+
+	
+	@GetMapping("/")
+	public String home() {
+		return "<h1>Welcome to TCS DevOps Architect Demo Application</h1>";
+	}
+}
---
+++ src/test/java/com/tcs/tcs_devops_architect_demo/TcsDevopsArchitectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.tcs_devops_architect_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcsDevopsArchitectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
